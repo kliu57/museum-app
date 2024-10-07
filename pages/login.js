3534f767fa5f1e0ecb58121140aa1e45c2fcb652
Katie Liu <98062538+kliu57@users.noreply.github.com>
2024-10-07 12:55:07 -0400

    Update login.js
    Changed message to the user

--- pages/login.js
+++ pages/login.js
@@ -40,7 +40,7 @@ export default function Login(props) {
       <Card bg="light">
         <Card.Body>
           <h2>Login</h2>
-          Enter your login information below:
+          Use guest, guest to login, or create your own account:
         </Card.Body>
       </Card>
 
